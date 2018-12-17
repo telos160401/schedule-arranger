eb082b6d592d60f55339c22b10b769ae235073e0
telos <telos160401@gmail.com>
2018-12-17 19:46:08 +0000

    データモデルの作成とユーザー情報の保存機能の追加2

---
+++ models/sequelize-loader.js
@@ -0,0 +1,13 @@
+'use strict';
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  'postgres://postgres:postgres@localhost/schedule_arranger',
+  {
+    logging: true,
+    operatorsAliases: false
+  });
+
+module.exports = {
+  database: sequelize,
+  Sequelize: Sequelize
+};
